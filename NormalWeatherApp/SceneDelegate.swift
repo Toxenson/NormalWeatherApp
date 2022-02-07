8f080860228e8092bf8235e9b6b98c9c2e220e6c
KamnevVladimir <trejlerfilms@gmail.com>
2022-02-07 15:55:20 +0300

    NWApp-5: страдай

--- NormalWeatherApp/SceneDelegate.swift
+++ NormalWeatherApp/SceneDelegate.swift
@@ -4,19 +4,18 @@
 //
 //  Created by Тоха on 04.02.2022.
 //
-// swiftlint:disable all
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-    // swiftlint:disable line_length
+ 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        // swiftlint:disable unused_optional_binding
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
